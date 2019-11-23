f136f4490eda6175644c9929a658019b7d1494c6
Olei Eto <teneeto@gmail.com>
2019-11-23 21:39:08 +0100

    initial setup

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to gif-app!');
+    expect(page.getTitleText()).toEqual('angular-test-gif-app app is running!');
   });
 
   afterEach(async () => {
